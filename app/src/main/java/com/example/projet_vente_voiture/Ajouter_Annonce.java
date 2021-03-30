9e87d80a8b348571b6c8f930fc795c7136c75e33
clink10Zero <47035153+clink10Zero@users.noreply.github.com>
2021-03-30 18:34:29 +0200

    activity crée ;)

---
+++ app/src/main/java/com/example/projet_vente_voiture/Ajouter_Annonce.java
@@ -0,0 +1,14 @@
+package com.example.projet_vente_voiture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ajouter_Annonce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_annonce);
+    }
+}
--- app/src/main/java/com/example/projet_vente_voiture/Mes_annonces.java
+++ app/src/main/java/com/example/projet_vente_voiture/Mes_annonces.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_detaille extends AppCompatActivity {
+public class Mes_annonces extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detaille);
+        setContentView(R.layout.activity_mes_annonces);
     }
 }
